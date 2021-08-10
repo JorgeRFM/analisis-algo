b4dfc6919e520b57cb88e3f36796962f93d90aaa
Jorge Franco <A01701980@itesm.mx>
2021-08-10 11:02:31 -0500

    first initial commit

---
+++ Parentesis.py
@@ -0,0 +1,45 @@
+# Python3 code to Check for 
+# balanced parentheses in an expression
+open_list = ["[","("]
+close_list = ["]",")"]
+  
+# Function to check parentheses
+def Parentesis(problem):
+    stack = []
+    for i in problem:
+        if i in open_list:
+            stack.append(i)
+        elif i in close_list:
+            pos = close_list.index(i)
+            if ((len(stack) > 0) and
+                (open_list[pos] == stack[len(stack)-1])):
+                stack.pop()
+            else:
+                return "Incorrect Syntax"
+    if len(stack) == 0:
+        return "Correct Syntax"
+    else:
+        return "Incorrect Syntax"
+  
+  
+# Driver code
+string = '()'
+print(string,"-", Parentesis(string))
+  
+string = '()()()()()()()()()'
+print(string,"-", Parentesis(string))
+  
+string = '([])[]()'
+print(string,"-",Parentesis(string))
+
+string = ')'
+print(string,"-",Parentesis(string))
+
+string = '()['
+print(string,"-",Parentesis(string))
+
+string = '(([))]'
+print(string,"-",Parentesis(string))
+
+string = '(((((((((((((((((((((((((((((((((('
+print(string,"-",Parentesis(string))
